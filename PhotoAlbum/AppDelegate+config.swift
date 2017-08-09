718c2f06992f5ccd37f41ed1f5457281fb3c6bd7
Shirly Manor <shirlymanor@Shirlys-MacBook-Pro.local>
2017-08-09 16:58:52 -0700

    first

---
+++ PhotoAlbum/AppDelegate+config.swift
@@ -0,0 +1,17 @@
+//
+//  Cloudinary+config.swift
+//
+
+import Foundation
+import Cloudinary
+
+extension AppDelegate {
+    static var cloudName: String {
+        return "shirly"
+    }
+
+
+    static var uploadPreset: String {
+        return "mypreset"
+    }
+}
